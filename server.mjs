158cc3376cb58e12287c35fcf041f2100f2ac942
kian_nrt7 <kiannosratian@gmail.com>
2024-03-31 02:50:23 +0330

    new app started

---
+++ server.mjs
@@ -0,0 +1,21 @@
+import  express  from "express";
+ import {dirname} from 'path';
+import { fileURLToPath } from "url";
+ const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+app.use(express.static(__dirname + '/01'))
+
+app.get("*",(req,res)=>{
+
+    res.sendFile( __dirname + "/index.html");
+
+})
+
+app.listen(port,()=>{
+    console.log(`server is runing on port ${port}`);
+})
+
+
+
